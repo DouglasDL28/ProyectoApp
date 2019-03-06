b779ab580510224da18776e5858da179d2372427
DouglasDL28 <dele18037@uvg.edu.gt>
2019-03-06 09:16:05 -0600

    Se agregó actividades de registro.

---
+++ app/src/main/java/com/example/douglasdeleon/horasuvg/StudentRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.douglasdeleon.horasuvg
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentRegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_register)
+    }
+}
