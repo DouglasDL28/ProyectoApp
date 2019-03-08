b9d57cc242661c83ba770e681a1f1b9f9090a177
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2019-03-08 09:59:36 -0600

    Cambios

--- app/src/main/java/com/example/douglasdeleon/horasuvg/LoginActivity.kt
+++ app/src/main/java/com/example/douglasdeleon/horasuvg/LoginActivity.kt
@@ -148,7 +148,7 @@ class LoginActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
             mAuthTask = UserLoginTask(emailStr, passwordStr)
             mAuthTask!!.execute(null as Void?)
 
-            mFirebaseAuth!!.createUserWithEmailAndPassword(emailStr,passwordStr).addOnCompleteListener{
+            mFirebaseAuth!!.signInWithEmailAndPassword(emailStr,passwordStr).addOnCompleteListener{
                 if (it.isSuccessful){
                     Toast.makeText(this,"Se ha iniciado sesión correctamente",Toast.LENGTH_LONG).show()
                 }else{
--- app/src/main/java/com/example/douglasdeleon/horasuvg/StudentRegisterActivity.kt
+++ app/src/main/java/com/example/douglasdeleon/horasuvg/StudentRegisterActivity.kt
@@ -2,11 +2,77 @@ package com.example.douglasdeleon.horasuvg
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_student_register.*
 
 class StudentRegisterActivity : AppCompatActivity() {
+
+    private var mFirebaseAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_register)
+
+        //Inicializa FireBase
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        ok_button.setOnClickListener {
+            register()
+        }
+    }
+
+    private fun register()  {
+
+        val emailStr = student_mail_textview.text.toString()
+        val passwordStr = student_password_textview.text.toString()
+
+        var cancel = false
+        var focusView: View? = null
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(passwordStr) && !isPasswordValid(passwordStr)) {
+            password.error = getString(R.string.error_invalid_password)
+            focusView = password
+            cancel = true
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(emailStr)) {
+            email.error = getString(R.string.error_field_required)
+            focusView = email
+            cancel = true
+        } else if (!isEmailValid(emailStr)) {
+            email.error = getString(R.string.error_invalid_email)
+            focusView = email
+            cancel = true
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView?.requestFocus()
+        } else {
+            mFirebaseAuth!!.createUserWithEmailAndPassword(emailStr,passwordStr).addOnCompleteListener{
+                if (it.isSuccessful){
+                    Toast.makeText(this,"Se ha creado el usuario correctamente", Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(this,"No se ha podido crear el usuario, revisa tus datos", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        //TODO: Replace this with your own logic
+        return email.contains("@uvg.edu.gt")
+    }
+
+    private fun isPasswordValid(password: String): Boolean {
+        //TODO: Replace this with your own logic
+        return password.length > 8
     }
 }
